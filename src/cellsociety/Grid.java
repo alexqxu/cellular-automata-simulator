15815ed3a243bfc048febbb55523930012ccdd5c
Maverick Chung <mc608@duke.edu>
2020-01-27 12:59:21 -0500

    made getNeighbors work with toroidal and default edges

--- src/cellsociety/Grid.java
+++ src/cellsociety/Grid.java
@@ -24,6 +24,7 @@ public class Grid {
 
     //FIXME grid needs to be toroidal for some cells (prey, conway) but empty boundaries for others (fire)
     //FIXME some cells need 8 neighbors (like conway)
+
     /**
      * Returns the 8 neighbors of the cell at r,c starting with North and rotating clockwise
      * Acts as though the grid is toroidal
@@ -34,10 +35,20 @@ public class Grid {
      */
     private int[] getNeighbors(int r, int c) {
         int[] ret = new int[8];
-        int[] dr = {-1,-1,0,1,1,1,0,-1};
-        int[] dc = {0,1,1,1,0,-1,-1,-1};
-        for (int i = 0; i < ret.length; i++) {
-            ret[i] = grid.get((r+dr[i]+getHeight())%getHeight()).get((c+dc[i]+getWidth())%getWidth()).getState(); 
+        int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+        int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+        if (getCell(r, c).getDefaultEdge() == -1) {
+            for (int i = 0; i < ret.length; i++) {
+                ret[i] = grid.get((r + dr[i] + getHeight()) % getHeight()).get((c + dc[i] + getWidth()) % getWidth()).getState();
+            }
+        } else {
+            for (int i = 0; i < ret.length; i++) {
+                try {
+                    ret[i] = grid.get(r+dr[i]).get(c+dc[i]).getState();
+                } catch (IndexOutOfBoundsException e) {
+                    ret[i] = getCell(r, c).getDefaultEdge();
+                }
+            }
         }
         return ret;
     }
