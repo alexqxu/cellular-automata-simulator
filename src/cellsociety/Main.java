095c254a19f1951a19ac9d3b81b4964eae068578
Alex Oesterling <axo@duke.edu>
2020-01-28 00:56:01 -0500

    began skeleton code for project. Created necessary functions from CRC cards and began filling in generic application methods as well as the drawGrid function.

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -5,19 +5,26 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.w3c.dom.css.Rect;
+
+import java.util.ArrayList;
 
 /**
  * @author: Alex Oesterling, axo
  */
 public class Main extends Application {
     public static final String TITLE = "Cell Simulator";
+    public static final int SIZE = 400;
     public static final int FRAMES_PER_SECOND = 120;
     public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
     public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
     private Stage myStage;
     private Grid myGrid;
+    private ArrayList<ArrayList<Rectangle>> cellGrid;
 
 
     /**
@@ -28,7 +35,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) { //throws exception?
         myStage = stage;
-        //set scene
+        myStage.setScene(createScene("file"));
         stage.setTitle(TITLE);
         stage.show();
         KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e->update(SECOND_DELAY));
@@ -38,10 +45,17 @@ public class Main extends Application {
         animation.play();
     }
 
-    private Scene setScene(){
+    private Scene createScene(String filename){
+        instantiateCellGrid();
+        loadConfigFile(filename);
         drawGrid();
         Scene scene = new Scene();
         return scene;
+    }
+    //FIXME how do we figure out the size of the cellgrid?
+    private void instantiateCellGrid() {
+        cellGrid = new ArrayList<ArrayList<Rectangle>>();
+
     }
 
     private void update(double elapsedTime){
@@ -53,10 +67,24 @@ public class Main extends Application {
     }
 
     public void loadConfigFile(String filename){
+        Grid =
         return;
     }
 
     public void drawGrid(){
+        Color[][] colorgrid = myGrid.getColorGrid();
+        for(int i = 0; i < colorgrid.length; i++){
+            cellGrid.add(new ArrayList<Rectangle>);
+            for(int j = 0; j < colorgrid[i].length; j++){
+                Rectangle cell = new Rectangle();
+                cell.setFill(colorgrid[i][j]);
+                cell.setWidth(SIZE/colorgrid[i].length);
+                cell.setHeight(SIZE/colorgrid.length);
+                cell.setX(j*cell.getWidth());
+                cell.setY(i*cell.getHeight());
+                cellGrid.get(i).add(cell);
+            }
+        }
         return;
     }
 
