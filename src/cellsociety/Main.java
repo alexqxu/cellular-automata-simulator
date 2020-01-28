a1f5f2b9c008746d4d4902ed66913b22c94a5e3a
Alex Oesterling <axo@duke.edu>
2020-01-28 00:31:31 -0500

    scene

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,18 +1,71 @@
 package cellsociety;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * @author: Alex Oesterling, axo
  */
 public class Main extends Application {
+    public static final String TITLE = "Cell Simulator";
+    public static final int FRAMES_PER_SECOND = 120;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
     private Stage myStage;
+    private Grid myGrid;
 
+
+    /**
+     * Start method. Runs game loop after setting up stage and scene data.
+     * @param stage the window in which the application runs
+     * @throws Exception
+     */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) { //throws exception?
         myStage = stage;
-        
+        //set scene
+        stage.setTitle(TITLE);
+        stage.show();
+        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e->update(SECOND_DELAY));
+        Timeline animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
+    }
+
+    private Scene setScene(){
+        drawGrid();
+        Scene scene = new Scene();
+        return scene;
+    }
+
+    private void update(double elapsedTime){
+        stepGrid();
+    }
+
+    public void setSpeed(){
+        return;
+    }
+
+    public void loadConfigFile(String filename){
+        return;
+    }
+
+    public void drawGrid(){
+        return;
+    }
+
+    public void drawCell(int x, int y, Color color){
+        return;
+    }
+
+    public void stepGrid(){
+        myGrid.update();
     }
 
     /**
