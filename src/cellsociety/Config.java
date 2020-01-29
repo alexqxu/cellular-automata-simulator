f0a07751d9e92e2098098af89a3358c1b1468814
Alex Xu <alex.xu@duke.edu>
2020-01-29 12:12:21 -0500

    Updated XML and Config

--- src/cellsociety/Config.java
+++ src/cellsociety/Config.java
@@ -5,17 +5,20 @@ import java.io.FileNotFoundException;
 public class Config {
     //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml";
 
-    private Grid myGrid;
+    //private Grid myGrid;
 
-    //public Grid loadFile(String path){
+    public Grid loadFile(String path){
+        Grid newGrid = new Grid();
+        return newGrid;
+    }
 
-    //}
+    //Take file input in
+    private void readFile throws FileNotFoundException(String path){
 
-    //private void readFile throws FileNotFoundException(String path){
+    }
 
-    //}
-
-    //private Grid createGrid(){
-
-    //}
+    private Grid createGrid(){
+        Grid newGrid = new Grid();
+        return newGrid;
+    }
 }
