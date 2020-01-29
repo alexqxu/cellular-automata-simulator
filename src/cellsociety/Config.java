93a67327e10de704834613c52a5b1793ca0e51f7
Alex Xu <alex.xu@duke.edu>
2020-01-29 11:40:28 -0500

    Added XML Document Sample

---
+++ src/cellsociety/Config.java
@@ -0,0 +1,21 @@
+package cellsociety;
+
+import java.io.FileNotFoundException;
+
+public class Config {
+    //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml";
+
+    private Grid myGrid;
+
+    //public Grid loadFile(String path){
+
+    //}
+
+    //private void readFile throws FileNotFoundException(String path){
+
+    //}
+
+    private Grid createGrid(){
+
+    }
+}
