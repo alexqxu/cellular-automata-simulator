eeabf7805dea2a8c9096fd919def4cde4de508c9
Alex Xu <alex.xu@duke.edu>
2020-01-31 17:49:21 -0500

    Added setup document methods for XML parser.

--- src/cellsociety/Config.java
+++ src/cellsociety/Config.java
@@ -1,6 +1,14 @@
 package cellsociety;
 
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Reads an XML file and sets up the Grid.
@@ -8,8 +16,26 @@ import java.io.FileNotFoundException;
  */
 public class Config {
     //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml";
-
     //private Grid myGrid;
+
+    private String myFilePath;
+    private File xmlFile;
+    private Document doc;
+
+
+    public Config(String filePath) throws ParserConfigurationException, SAXException, IOException {
+        myFilePath = filePath;
+        setupDocument();
+    }
+
+    private void setupDocument() throws IOException, SAXException, ParserConfigurationException {
+        xmlFile = new File(myFilePath);
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        doc = builder.parse(xmlFile);
+        doc.getDocumentElement().normalize();                                   //optional, might remove later.
+    }
+
 
     public Grid loadFile(String path){
         Grid newGrid = new Grid();
