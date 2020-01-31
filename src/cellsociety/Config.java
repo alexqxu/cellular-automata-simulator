380294052e209ffee8255b58eefab30b77928927
Alex Xu <alex.xu@duke.edu>
2020-01-31 15:19:15 -0500

    Updated Game of Life XML and Config

--- src/cellsociety/Config.java
+++ src/cellsociety/Config.java
@@ -2,6 +2,10 @@ package cellsociety;
 
 import java.io.FileNotFoundException;
 
+/**
+ * Reads an XML file and sets up the Grid.
+ * @author Alex Xu aqx
+ */
 public class Config {
     //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml";
 
@@ -13,9 +17,9 @@ public class Config {
     }
 
     //Take file input in
-    private void readFile throws FileNotFoundException(String path){
+    //private void readFile throws FileNotFoundException(String path){
 
-    }
+    //}
 
     private Grid createGrid(){
         Grid newGrid = new Grid();
