e86ab0986f2542970500b98cabc50ffa4bcf065a
Alex Xu <alex.xu@duke.edu>
2020-01-31 22:53:18 -0500

    Implemented extract config info

--- src/cellsociety/Config.java
+++ src/cellsociety/Config.java
@@ -2,6 +2,9 @@ package cellsociety;
 
 import javafx.scene.paint.Color;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -18,7 +21,9 @@ import java.util.Map;
  * @author Alex Xu aqx
  */
 public class Config {
-    //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml"; //Delete Later.
+    //public static final String XML_CONFIG_FILEPATH = "GameofLife.xml"; //Delete Later, since the filepath is now passed in.
+    private String ConfigNodeName = "ConfigInfo";
+
     private Grid myGrid;
 
     private String myFilePath;
@@ -27,11 +32,13 @@ public class Config {
 
     private String myTitle;
     private String myAuthor;
-    private String myWidth;
-    private String myHeight;
+
     private Map<String, Color> myStates;
     private String defaultState;
+
     private int mySpeed;
+    private int myWidth;
+    private int myHeight;
 
     /**
      * Constructor for the Config object. Sets the filepath and sets up the documentBuilder.
@@ -43,6 +50,8 @@ public class Config {
     public Config(String filePath) throws ParserConfigurationException, SAXException, IOException {
         myFilePath = filePath;
         setupDocument();
+        System.out.println("Document Setup Complete");                                                                  //Debugging Purposes Only.
+        extractConfigInfo();
     }
 
     private void setupDocument() throws IOException, SAXException, ParserConfigurationException {
@@ -50,8 +59,61 @@ public class Config {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         doc = builder.parse(xmlFile);
-        doc.getDocumentElement().normalize();                                   //optional, might remove later.
+        doc.getDocumentElement().normalize();                                                                           //optional, might remove later.
     }
+
+    /**
+     * Extracts all information in the XML Document that lies within <ConfigInfo>.
+     */
+    private void extractConfigInfo(){
+        NodeList configNodeList = doc.getElementsByTagName(ConfigNodeName);
+        Node configNode = configNodeList.item(0);
+
+        if(configNode.getNodeType() == Node.ELEMENT_NODE) {
+            Element configElement = (Element) configNode;
+            extractTitle(configElement);
+            extractAuthor(configElement);
+            extractDimensions(configElement);
+            extractStates(configElement);
+        }
+    }
+
+    private void extractTitle(Element startingElement){
+        myTitle = startingElement.getElementsByTagName("Title").item(0).getTextContent();
+        System.out.println("Simulation Name: "+ myTitle);                                                               //For Debugging Purposes.
+    }
+    private void extractAuthor(Element startingElement){
+        myAuthor = startingElement.getElementsByTagName("Author").item(0).getTextContent();
+        System.out.println("Author: "+ myAuthor);
+    }
+
+    private void extractDimensions(Element startingElement){
+        Node dimensionsNode = startingElement.getElementsByTagName("Dimensions").item(0);
+        if(dimensionsNode.getNodeType() == Node.ELEMENT_NODE){
+            Element dimensionsElement = (Element) dimensionsNode;
+            extractHeight(dimensionsElement);
+            extractWidth(dimensionsElement);
+            extractSpeed(dimensionsElement);
+        }
+    }
+
+    private void extractSpeed(Element dimensionsElement) {
+        mySpeed = Integer.parseInt(dimensionsElement.getElementsByTagName("Speed").item(0).getTextContent().trim());
+    }
+
+    private void extractWidth(Element dimensionsElement) {
+        myWidth = Integer.parseInt(dimensionsElement.getElementsByTagName("Width").item(0).getTextContent().trim());
+    }
+
+    private void extractHeight(Element dimensionsElement) {
+        myHeight = Integer.parseInt(dimensionsElement.getElementsByTagName("Height").item(0).getTextContent().trim());
+    }
+
+    private void extractStates(Element startingElement){
+        
+    }
+
+
 
     public String getTitle(){
         return myTitle;
@@ -59,10 +121,10 @@ public class Config {
     public String getAuthor(){
         return myAuthor;
     }
-    public String getWidth(){
+    public int getWidth(){
         return myWidth;
     }
-    public String getHeight(){
+    public int getHeight(){
         return myHeight;
     }
     public Map<String, Color> getStates(){
