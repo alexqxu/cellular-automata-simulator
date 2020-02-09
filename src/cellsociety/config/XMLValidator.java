2a6bb25da4e6825f12cdb65a3efa977afb2d294d
Alex Xu <alex.xu@duke.edu>
2020-02-09 12:52:49 -0500

    Added fully functional XML Structure Testing

--- src/cellsociety/config/XMLValidator.java
+++ src/cellsociety/config/XMLValidator.java
@@ -1,4 +1,33 @@
 package cellsociety.config;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+import cellsociety.exceptions.InvalidXMLStructureException;
+import org.xml.sax.SAXException;
+
 public class XMLValidator {
+    public static final String XSD_SCHEMA_FILEPATH = "src\\cellsociety\\config\\schema.xsd";
+
+    /**
+     * Validates a XML file against the XSD file that is given as part of the program.
+     * @param xmlFile
+     * @return true if the document structure is valid, false otherwise.
+     */
+    public static boolean validateXMLStructure(File xmlFile){
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = factory.newSchema(new File(XSD_SCHEMA_FILEPATH));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xmlFile));
+        }catch(IOException | SAXException e){
+            return false;
+        }
+        return true;
+    }
 }
--- src/cellsociety/exceptions/InvalidXMLStructureException.java
+++ src/cellsociety/exceptions/InvalidXMLStructureException.java
@@ -5,4 +5,7 @@ public class InvalidXMLStructureException extends RuntimeException{
     public InvalidXMLStructureException(Throwable cause, String message) {
         super(message, cause);
     }
+    public InvalidXMLStructureException(String message, Object... values) {
+        super(String.format(message, values));
+    }
 }
