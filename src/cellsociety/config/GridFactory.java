a92f883d7b87fcbfd1c86f55944f636d22648db5
Alex Xu <alex.xu@duke.edu>
2020-02-14 18:17:06 -0500

    Created GridFactory class

---
+++ src/cellsociety/config/GridFactory.java
@@ -0,0 +1,8 @@
+package cellsociety.config;
+
+/**
+ * This is a refactored class for the Code Masterpiece. The purpose of this class is to handle gridmaking, which is now separated
+ * from the parsing portion of the configuration component.
+ */
+public class GridFactory {
+}
